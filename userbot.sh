b557e98e57f4149d486a8ef829f66ee35bb5f931
msechen <74814336+msechen@users.noreply.github.com>
2022-04-01 23:12:27 +0800

    Update userbot.sh

--- userbot.sh
+++ userbot.sh
@@ -1,3 +1,4 @@
+rm -rf diybot.sh &&rm -rf user.sh&&rm -rf userbot.sh
 echo -e "======================== 1. 部署自定义jbot-diy机器人 ========================\n"
 if [ -d "/jd" ]; then root=/jd; else root=/ql; fi; if [ -f $root/diybot.sh ]; then rm -f $root/diybot.sh; fi; cd $root; wget https://raw.githubusercontent.com/msechen/JD_Diy/main/shell/diybot.sh; bash diybot.sh
 
